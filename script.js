3a587fbd1ee32ed9bbbd36a6d585d00c8bd87371
Karen <karen.oganesyan2004@gmail.com>
2022-10-09 22:20:54 +0300

    add movies and raitings in personalMovieDB

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", "");
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: NaN,
-    actors: NaN,
+    movies: {},
+    actors: {},
     genres: [],
     privat: false,    
 };
+
+const movies1 = prompt("Один из просмотренных последних фильмов?","");
+const rait1 = prompt("На сколько его оцените","");
+const movies2 = prompt("Один из просмотренных последних фильмов?","");
+const rait2 = prompt("На сколько его оцените","");
+
+personalMovieDB.movies[movies1] = rait1;
+personalMovieDB.movies[movies2] = rait2;
+
+console.log(personalMovieDB);
